e085c11404b9b368b99b8fb015c4c6a93abcaf99
Rune Morling <ermo@serpentos.com>
2025-05-01 22:47:09 +0200

    deployment: Add rudimentary deployment templates
    This is what I used to get started with my own LAN infra setup.
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

---
+++ deployment/source-me-to-deploy-rust-services.bash
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Crude AerynOS Rust service deployment script
+
+create_service_user () {
+  local _svc="$1"
+
+  # create service users and home dirs
+  sudo useradd --create-home --home-dir /srv/${_svc}-rs --user-group --system ${_svc}-rs
+  # add invoking user to service groups
+  sudo usermod -a -G ${_svc}-rs ${USER}
+  echo "Now Relog to ensure that your user is part of the ${_svc}-rs group."
+}
+
+deploy_service () {
+  local _svc="$1"
+  # necessary for setgid on dirs being inherited by newly created files
+  umask 0002  
+  # set correct permissions for service home dir
+  sudo chmod -c g+rwsX /srv/${_svc}-rs
+  sudo chmod -Rc g+rwX /srv/${_svc}-rs
+  # set up state dir to be ready for the .privkey private key in bytes format
+  mkdir -pv /srv/${_svc}-rs/${_svc}/state
+  # copy binaries to service home dirs
+  cp -v target/release/${_svc} /srv/${_svc}-rs/${_svc}/${_svc}.app
+  chmod -c a+x,g+w /srv/${_svc}-rs/${_svc}/${_svc}.app
+  # reset permissions
+  sudo chown -Rc ${_svc}-rs:${_svc}-rs /srv/${_svc}-rs
+  sudo chmod -Rc g+rwX /srv/${_svc}-rs/${_svc}
+  # copy .service definitions to /etc/systemd/system/
+  sudo cp -v aos-${_svc}-rs.service /etc/systemd/system/
+  sudo systemctl daemon-reload
+  echo "Now set up config.toml files and private/public keys."
+}
+
+help() {
+echo -e "
+  Deployment procedure:
+  - source deploy-rust-services.bash
+  - create_service_user <the service> # (one of avalanche|summit|vessel)
+  - relog to update service group membership
+  - source deploy-rust-services.bash
+  - deploy_service <the service>
+  - copy private key to /srv/<the service>-rs/state/.privkey
+  - edit and copy service config.toml to /srv/<the service>-rs/
+"
+}
+
+help
