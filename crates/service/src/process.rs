732d728d1fb874f013440e6bf42d8b7e278fb7d6
Cory Forsstrom <cforsstrom18@gmail.com>
2025-05-02 07:59:11 -0700

    Add trace logging to command outputs

--- crates/service/src/process.rs
+++ crates/service/src/process.rs
@@ -1,7 +1,11 @@
 //! Execute commands
-use std::{io, process::Stdio};
+use std::{
+    io,
+    process::{ExitStatus, Stdio},
+};
 
 use tokio::process::{ChildStderr, ChildStdout, Command};
+use tracing::trace;
 
 /// Execute the command and return it's stdout output
 pub async fn output(command: impl AsRef<str>, f: impl FnOnce(&mut Command) -> &mut Command) -> Result<String, Error> {
@@ -14,38 +18,15 @@ pub async fn output(command: impl AsRef<str>, f: impl FnOnce(&mut Command) -> &m
         .await
         .map_err(|err| Error::Io(command.to_string(), err))?;
 
+    let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+    let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+
+    trace!(command, stdout, stderr, status = %output.status, "Command finished");
+
     if !output.status.success() {
-        let error = String::from_utf8_lossy(&output.stderr).to_string();
-
-        if let Some(code) = output.status.code() {
-            Err(Error::FailedOutputWithStatus(command.to_string(), code, error))
-        } else {
-            Err(Error::FailedOutput(command.to_string(), error))
-        }
+        Err(Error::FailedWithOutput(command.to_string(), output.status, stderr))
     } else {
-        Ok(String::from_utf8_lossy(&output.stdout).to_string())
-    }
-}
-
-/// Execute the command, discarding its output
-pub async fn execute(command: impl AsRef<str>, f: impl FnOnce(&mut Command) -> &mut Command) -> Result<(), Error> {
-    let command = command.as_ref();
-
-    let mut process = Command::new(command);
-
-    let status = f(&mut process)
-        .status()
-        .await
-        .map_err(|err| Error::Io(command.to_string(), err))?;
-
-    if !status.success() {
-        if let Some(code) = status.code() {
-            Err(Error::FailedWithStatus(command.to_string(), code))
-        } else {
-            Err(Error::Failed(command.to_string()))
-        }
-    } else {
-        Ok(())
+        Ok(stdout)
     }
 }
 
@@ -77,12 +58,10 @@ where
 
     let _ = task.await;
 
+    trace!(command, %status, "Command finished");
+
     if !status.success() {
-        if let Some(code) = status.code() {
-            Err(Error::FailedWithStatus(command.to_string(), code))
-        } else {
-            Err(Error::Failed(command.to_string()))
-        }
+        Err(Error::Failed(command.to_string(), status))
     } else {
         Ok(())
     }
@@ -95,15 +74,9 @@ pub enum Error {
     #[error("{0} failed: {1}")]
     Io(String, io::Error),
     /// Process failed
-    #[error("{0} exited with failure")]
-    Failed(String),
-    /// Process failed with exit code
-    #[error("{0} failed with exit status {1}")]
-    FailedWithStatus(String, i32),
+    #[error("{0} failed: {1}")]
+    Failed(String, ExitStatus),
     /// Process failed with provided stderr
-    #[error("{0} exited with failure: {1}")]
-    FailedOutput(String, String),
-    /// Process failed with exit code & provided stderr
-    #[error("{0} failed with exit status {1}: {2}")]
-    FailedOutputWithStatus(String, i32, String),
+    #[error("{0} failed: {1}: {2}")]
+    FailedWithOutput(String, ExitStatus, String),
 }
