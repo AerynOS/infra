a38cb7adc1a8d60f298cda4d18d9e54dda8b5088
Jonas Platte <jplatte+git@posteo.de>
2025-05-15 22:09:27 +0200

    Move template filters into their own module

--- crates/summit/src/templates.rs
+++ crates/summit/src/templates.rs
@@ -1,11 +1,9 @@
 use axum::response::Html;
 use http::StatusCode;
-use minijinja::{Environment, Value, value::ViaDeserialize};
+use minijinja::Environment;
 use serde::Serialize;
-use service::{Endpoint, endpoint};
 
-use crate::Project;
-
+mod filters;
 #[cfg_attr(
     all(feature = "templates-bundled", not(feature = "templates-autoreload")),
     path = "templates/bundled.rs"
@@ -17,10 +15,10 @@ pub type Response = axum::response::Result<Html<String>>;
 
 fn env() -> Environment<'static> {
     let mut env = Environment::new();
-    env.add_filter("repository", repository_filter);
-    env.add_filter("profile", profile_filter);
-    env.add_filter("endpoint", endpoint_filter);
-    env.add_filter("format_duration", format_duration_filter);
+    env.add_filter("repository", filters::repository);
+    env.add_filter("profile", filters::profile);
+    env.add_filter("endpoint", filters::endpoint);
+    env.add_filter("format_duration", filters::format_duration);
 
     env
 }
@@ -46,39 +44,4 @@ where
     };
 
     implementation::with_environment(do_render)
-}
-
-fn repository_filter(projects: ViaDeserialize<Vec<Project>>, id: i64) -> Option<Value> {
-    projects
-        .iter()
-        .find_map(|p| p.repositories.iter().find(|r| r.id == id.into()))
-        .map(Value::from_serialize)
-}
-
-fn profile_filter(projects: ViaDeserialize<Vec<Project>>, id: i64) -> Option<Value> {
-    projects
-        .iter()
-        .find_map(|p| p.profiles.iter().find(|p| p.id == id.into()))
-        .map(Value::from_serialize)
-}
-
-fn endpoint_filter(
-    endpoints: ViaDeserialize<Vec<Endpoint>>,
-    id: ViaDeserialize<Option<endpoint::Id>>,
-) -> Option<Value> {
-    let id = id.0?;
-
-    endpoints.iter().find(|e| e.id == id).map(Value::from_serialize)
-}
-
-fn format_duration_filter(value: Value) -> Result<Value, minijinja::Error> {
-    let secs = match value.as_i64() {
-        Some(s) => s,
-        None => return Ok(Value::from("")),
-    };
-    let hours = secs / 3600;
-    let minutes = (secs % 3600) / 60;
-    let seconds = secs % 60;
-
-    Ok(Value::from(format!("{hours:02}h {minutes:02}m {seconds:02}s")))
 }
