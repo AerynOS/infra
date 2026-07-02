fdb4fcc5d8c6132f07e6739549ae2cd8cbd06421
Cory Forsstrom <cforsstrom18@gmail.com>
2026-07-02 14:01:01 -0700

    Migrate vessel commands over summit stream
    By forcing commands over summit, we can remove the
    auth service from vessel and limit its grpc surface
    to just package uploads via single use / scoped tokens.
    
    Summit can then become the single source of truth
    for all auth / session management.
    
    This also adds all the plumbing necessary to easily
    invoke these same commands from a future summit
    web admin dashboard via the connected stream.

--- crates/vessel/src/grpc.rs
+++ crates/vessel/src/grpc.rs
@@ -1,19 +1,15 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use color_eyre::eyre::{OptionExt as _, Report, bail, eyre};
 use futures_util::TryStreamExt;
 use prost::Message;
 use service::{
-    Session,
     grpc::{
         self,
         proto::{
-            self,
             summit::builder_stream::BuildFinished,
             vessel::{
-                AddTagRequest, CommandResponse, RemoveTagRequest, UpdateStreamRequest, UpgradeFormatRequest,
-                UploadRequest, upgrade_format,
+                UploadRequest,
                 vessel_service_server::{VesselService as GrpcVesselService, VesselServiceServer},
             },
         },
@@ -21,15 +17,10 @@ use service::{
     token::VerifiedToken,
 };
 use snafu::{OptionExt, ResultExt, Snafu};
-use tokio::sync::{mpsc, oneshot};
-use tracing::{Span, error, info, warn};
+use tokio::sync::mpsc;
+use tracing::{Span, info, warn};
 
-use crate::{
-    channel::{self, FormatUpgrade},
-    upload, worker,
-};
-
-pub use service::grpc::auth::service as auth_service;
+use crate::{upload, worker};
 
 pub fn vessel_service(state: service::State, worker: worker::Sender) -> VesselServiceServer<VesselService> {
     VesselServiceServer::new(VesselService {
@@ -57,42 +48,6 @@ impl GrpcVesselService for VesselService {
         let state = self.state.clone();
 
         grpc::handle(request, async move |request| upload(state, request).await).await
-    }
-
-    async fn update_stream(
-        &self,
-        request: tonic::Request<UpdateStreamRequest>,
-    ) -> Result<tonic::Response<CommandResponse>, tonic::Status> {
-        let state = self.state.clone();
-
-        grpc::handle(request, async move |request| update_stream(state, request).await).await
-    }
-
-    async fn add_tag(
-        &self,
-        request: tonic::Request<AddTagRequest>,
-    ) -> Result<tonic::Response<CommandResponse>, tonic::Status> {
-        let state = self.state.clone();
-
-        grpc::handle(request, async move |request| add_tag(state, request).await).await
-    }
-
-    async fn remove_tag(
-        &self,
-        request: tonic::Request<RemoveTagRequest>,
-    ) -> Result<tonic::Response<CommandResponse>, tonic::Status> {
-        let state = self.state.clone();
-
-        grpc::handle(request, async move |request| remove_tag(state, request).await).await
-    }
-
-    async fn upgrade_format(
-        &self,
-        request: tonic::Request<UpgradeFormatRequest>,
-    ) -> Result<tonic::Response<CommandResponse>, tonic::Status> {
-        let state = self.state.clone();
-
-        grpc::handle(request, async move |request| upgrade_format(state, request).await).await
     }
 }
 
@@ -146,253 +101,10 @@ async fn upload(state: Arc<State>, request: tonic::Request<tonic::Streaming<Uplo
     Ok(())
 }
 
-#[tracing::instrument(
-    skip_all,
-    fields(
-        channel = %request.get_ref().channel,
-        stream = ?request.get_ref().stream(),
-        version = %request.get_ref().version,
-    )
-)]
-async fn update_stream(
-    state: Arc<State>,
-    request: tonic::Request<UpdateStreamRequest>,
-) -> Result<CommandResponse, Error> {
-    let session = request.extensions().get::<Session>().ok_or(Error::NoActiveSession)?;
-
-    info!(
-        session_id = %session.id,
-        client = %session.client,
-        "Update stream request received"
-    );
-
-    let body = request.into_inner();
-
-    let (sender, receiver) = oneshot::channel();
-
-    let validate = || -> Result<_, Report> {
-        let stream = match body.stream() {
-            proto::vessel::Stream::Unknown => bail!("unknown stream"),
-            proto::vessel::Stream::Volatile => channel::version::Stream::Volatile,
-            proto::vessel::Stream::Unstable => channel::version::Stream::Unstable,
-        };
-        let version =
-            channel::Version::try_from(body.version).map_err(|err| eyre!("failed to parse version: {err}"))?;
-
-        Ok(worker::Message::ChannelCommand {
-            channel: body.channel,
-            command: channel::Command::UpdateStream { stream, version },
-            response: sender,
-        })
-    };
-
-    match validate() {
-        Ok(command) => {
-            let _ = state.worker.send(command);
-        }
-        Err(error) => {
-            return Ok(CommandResponse {
-                success: false,
-                error: Some(format!("{error:#}")),
-            });
-        }
-    }
-
-    match receiver.await? {
-        Ok(_) => Ok(CommandResponse {
-            success: true,
-            error: None,
-        }),
-        Err(error) => Ok(CommandResponse {
-            success: false,
-            error: Some(format!("{error:#}")),
-        }),
-    }
-}
-
-#[tracing::instrument(
-    skip_all,
-    fields(
-        channel = %request.get_ref().channel,
-        tag = ?request.get_ref().tag,
-        history = %request.get_ref().history,
-    )
-)]
-async fn add_tag(state: Arc<State>, request: tonic::Request<AddTagRequest>) -> Result<CommandResponse, Error> {
-    let session = request.extensions().get::<Session>().ok_or(Error::NoActiveSession)?;
-
-    info!(
-        session_id = %session.id,
-        client = %session.client,
-        "Add tag request received"
-    );
-
-    let body = request.into_inner();
-
-    let (sender, receiver) = oneshot::channel();
-
-    let validate = || -> Result<_, Report> {
-        let tag = channel::version::Identifier::new(&body.tag)?.into();
-        let history = channel::version::Identifier::new(&body.history)?.into();
-
-        Ok(worker::Message::ChannelCommand {
-            channel: body.channel,
-            command: channel::Command::AddTag { tag, history },
-            response: sender,
-        })
-    };
-
-    match validate() {
-        Ok(command) => {
-            let _ = state.worker.send(command);
-        }
-        Err(error) => {
-            return Ok(CommandResponse {
-                success: false,
-                error: Some(format!("{error:#}")),
-            });
-        }
-    }
-
-    match receiver.await? {
-        Ok(_) => Ok(CommandResponse {
-            success: true,
-            error: None,
-        }),
-        Err(error) => Ok(CommandResponse {
-            success: false,
-            error: Some(format!("{error:#}")),
-        }),
-    }
-}
-
-#[tracing::instrument(
-    skip_all,
-    fields(
-        channel = %request.get_ref().channel,
-        tag = ?request.get_ref().tag,
-    )
-)]
-async fn remove_tag(state: Arc<State>, request: tonic::Request<RemoveTagRequest>) -> Result<CommandResponse, Error> {
-    let session = request.extensions().get::<Session>().ok_or(Error::NoActiveSession)?;
-
-    info!(
-        session_id = %session.id,
-        client = %session.client,
-        "Remove tag request received"
-    );
-
-    let body = request.into_inner();
-
-    let (sender, receiver) = oneshot::channel();
-
-    let validate = || -> Result<_, Report> {
-        let tag = channel::version::Identifier::new(&body.tag)?.into();
-
-        Ok(worker::Message::ChannelCommand {
-            channel: body.channel,
-            command: channel::Command::RemoveTag { tag },
-            response: sender,
-        })
-    };
-
-    match validate() {
-        Ok(command) => {
-            let _ = state.worker.send(command);
-        }
-        Err(error) => {
-            return Ok(CommandResponse {
-                success: false,
-                error: Some(format!("{error:#}")),
-            });
-        }
-    }
-
-    match receiver.await? {
-        Ok(_) => Ok(CommandResponse {
-            success: true,
-            error: None,
-        }),
-        Err(error) => Ok(CommandResponse {
-            success: false,
-            error: Some(format!("{error:#}")),
-        }),
-    }
-}
-
-#[tracing::instrument(
-    skip_all,
-    fields(
-        channel = %request.get_ref().channel,
-        format,
-    )
-)]
-async fn upgrade_format(
-    state: Arc<State>,
-    request: tonic::Request<UpgradeFormatRequest>,
-) -> Result<CommandResponse, Error> {
-    let body = request.into_inner();
-
-    let format_upgrade = body.format.and_then(|f| match f.format? {
-        upgrade_format::Format::Legacy(request) => Some(FormatUpgrade::Legacy {
-            tag_name: request.tag_name,
-        }),
-    });
-
-    if let Some(format) = format_upgrade.as_ref() {
-        Span::current().record("format", format.to_string());
-    }
-
-    info!("Upgrade format request received");
-
-    let (sender, receiver) = oneshot::channel();
-
-    let validate = || -> Result<_, Report> {
-        let format_upgrade = format_upgrade.ok_or_eyre("missing format")?;
-
-        Ok(worker::Message::ChannelCommand {
-            channel: body.channel,
-            command: channel::Command::FormatUpgrade(format_upgrade),
-            response: sender,
-        })
-    };
-
-    match validate() {
-        Ok(command) => {
-            let _ = state.worker.send(command);
-        }
-        Err(err) => {
-            error!(error = format!("{err:#}"), "Upgrade format request validation failed");
-
-            return Ok(CommandResponse {
-                success: false,
-                error: Some(format!("{err:#}")),
-            });
-        }
-    }
-
-    match receiver.await? {
-        Ok(_) => Ok(CommandResponse {
-            success: true,
-            error: None,
-        }),
-        Err(err) => {
-            error!(error = format!("{err:#}"), "Upgrade format command failed");
-
-            Ok(CommandResponse {
-                success: false,
-                error: Some(format!("{err:#}")),
-            })
-        }
-    }
-}
-
 #[derive(Debug, Snafu)]
 enum Error {
     #[snafu(display("Token missing from request"))]
     MissingRequestToken,
-    #[snafu(display("No active session"))]
-    NoActiveSession,
     #[snafu(display("Invalid upload token"))]
     InvalidUploadToken,
     #[snafu(display("Failed to send task to worker"))]
@@ -403,12 +115,8 @@ enum Error {
     GrpcRequest { source: tonic::Status },
     #[snafu(display("Missing upload token body from upload stream"))]
     MissingUploadTokenRequest,
-    #[snafu(display("Missing upload signature from upload stream"))]
-    MissingUploadSignatureRequest,
     #[snafu(display("Failed to decode upload token body"))]
     DecodeUploadTokenRequest { source: prost::DecodeError },
-    #[snafu(transparent)]
-    OneshotRecv { source: oneshot::error::RecvError },
     #[snafu(display("Failed to save packages"))]
     SavePackages { source: upload::Error },
 }
@@ -416,12 +124,12 @@ enum Error {
 impl From<Error> for tonic::Status {
     fn from(error: Error) -> Self {
         match error {
-            Error::MissingRequestToken | Error::NoActiveSession => tonic::Status::unauthenticated(""),
+            Error::MissingRequestToken => tonic::Status::unauthenticated(""),
             Error::InvalidUploadToken => tonic::Status::permission_denied(""),
-            Error::MissingUploadTokenRequest
-            | Error::MissingUploadSignatureRequest
-            | Error::DecodeUploadTokenRequest { .. } => tonic::Status::invalid_argument(""),
-            Error::SendWorker { .. } | Error::OneshotRecv { .. } => tonic::Status::internal(""),
+            Error::MissingUploadTokenRequest | Error::DecodeUploadTokenRequest { .. } => {
+                tonic::Status::invalid_argument("")
+            }
+            Error::SendWorker { .. } => tonic::Status::internal(""),
             Error::GrpcRequest { source } => source,
             Error::SavePackages { source } => match source {
                 upload::Error::Sha256Mismatch { .. }
